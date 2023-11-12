8282b2d93b69231b887707a13064596ec4e690df
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-11-11 20:40:10 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    i=0
+    while i < length:
+        if len(fib_sequence) == 0:
+            fib_sequence.append(0)
+        elif len(fib_sequence) == 1:
+            fib_sequence.append(1)
+        else:
+            fib_sequence.append(fib_sequence[-1]+fib_sequence[-2])
+        i+=1
+    print(fib_sequence)
